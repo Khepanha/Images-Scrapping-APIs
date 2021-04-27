41519c3680f817d4e056174ea5746dc4fac2fd82
Khepanha <khepanha18@kit.edu.kh>
2021-04-27 17:03:23 +0700

    Add pexels.py and pixabay.py

--- Pexels.py
+++ Pexels.py
@@ -2,7 +2,9 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
 import urllib.request as urllib2
+from pexels_api import API
 from termcolor import colored
+# from urllib.request import Request, urlopen
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
@@ -11,26 +13,31 @@ client = gspread.authorize(creds)
 
 sheet = client.open("Scrap Unsplash API python").sheet1
 undefined_word = client.open("Scrap Unsplash API python").worksheet('Undefined Word')
+
 words = sheet.col_values(2)
 words.pop(0)
 row = 1
 for word in words:
     img = []
-    req = requests.get("https://api.unsplash.com/search/photos?page=1&query="+word+"&client_id=WnqByO4bcBwmoasQWIT_aXxLNDlAq-QQXXBcWDRcg0I")
+    req = requests.get("https://api.pexels.com/v1/search?query="+word, headers={'Authorization': '563492ad6f917000010000010afc00429f794a0ab73ca37dc4666d05'})
     data = req.json()
+
     filename = str(word) + ".jpg"
-    for res in data['results']:
+    for res in data['photos']:
         if res['height'] > res['width']:
             img.append(res)
         else: continue
     if img != []:
-        last_res = max(img,key=lambda item:item['likes'])
-        urllib2.urlretrieve(last_res['urls']['small'], "./images/"+filename)
+        last_res = max(img,key=lambda item:item['width'])
+        img_req = urllib2.build_opener()
+        img_req.addheaders = [{'User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64)'}]
+        urllib2.install_opener(img_req)
+        urllib2.urlretrieve(last_res['src']['medium'], "./images/"+filename)
         print (word + " Downloaded! ✔")
+        # print (last_res['src']['large'])
     else:
         row += 1
         undefined_word.update_cell(row,1,f"{word}")
         undefined_word.update_cell(row,2,"Undefined")
-        print (colored(word + " Undefined! X",'red'))
-
+        print (colored(word + " Undefined! X", 'red'))
 print (colored('------------------\nSucessfully Download all your images!:)', 'green'))
--- Pixabay.py
+++ Pixabay.py
@@ -1,8 +1,9 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from termcolor import colored
 import requests
 import urllib.request as urllib2
-from termcolor import colored
+import shutil
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
@@ -11,26 +12,32 @@ client = gspread.authorize(creds)
 
 sheet = client.open("Scrap Unsplash API python").sheet1
 undefined_word = client.open("Scrap Unsplash API python").worksheet('Undefined Word')
+
 words = sheet.col_values(2)
 words.pop(0)
 row = 1
 for word in words:
     img = []
-    req = requests.get("https://api.unsplash.com/search/photos?page=1&query="+word+"&client_id=WnqByO4bcBwmoasQWIT_aXxLNDlAq-QQXXBcWDRcg0I")
+    req = requests.get("https://pixabay.com/api/?key=21280539-53361c7eaa084b788ec5c194e&q="+word+"&image_type=photo")
     data = req.json()
+
     filename = str(word) + ".jpg"
-    for res in data['results']:
-        if res['height'] > res['width']:
+    for res in data['hits']:
+        # if res['imageHeight'] > res['imageWidth'] and word.lower() in res['tags'] or word.lower()+"s" in res['tags']:
+        if res['imageHeight'] > res['imageWidth']:
             img.append(res)
         else: continue
     if img != []:
         last_res = max(img,key=lambda item:item['likes'])
-        urllib2.urlretrieve(last_res['urls']['small'], "./images/"+filename)
+        img_req = urllib2.build_opener()
+        img_req.addheaders = [{'User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64)'}]
+        urllib2.install_opener(img_req)
+        urllib2.urlretrieve(last_res['largeImageURL'], "./images/"+filename)
         print (word + " Downloaded! ✔")
     else:
         row += 1
         undefined_word.update_cell(row,1,f"{word}")
         undefined_word.update_cell(row,2,"Undefined")
-        print (colored(word + " Undefined! X",'red'))
+        print (colored(word + " Undefined! X", 'red'))
 
-print (colored('------------------\nSucessfully Download all your images!:)', 'green'))
+print (colored('------------------\nSucessfully Download all your images!:)','green'))
