921da837b1c4823b11c06d3d3f06268ce23511e6
sinsovannmony <sinsovannmony18@kit.edu.kh>
2021-05-03 16:02:00 +0700

    handle time request limit

--- Unsplash.py
+++ Unsplash.py
@@ -4,6 +4,7 @@ import requests
 import urllib.request as urllib2
 from termcolor import colored
 import os , dotenv
+import datetime , time
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
@@ -21,24 +22,28 @@ row = 1
 start = os.environ.get('start')
 # for word in words:
 for i in range(int(start),len(words)):
-    img = []
-    req = requests.get("https://api.unsplash.com/search/photos?page=1&query="+words[i]+"&client_id=m64NRbuNadSCiuSf4wx_LbBhYe1nY6sdJIqkkPSEovA")
-    data = req.json()
-    filename = str(words[i]) + ".jpg"
-    for res in data['results']:
-        if res['height'] > res['width']:
-            img.append(res)
-        else: continue
-    if img != []:
-        last_res = max(img,key=lambda item:item['likes'])
-        urllib2.urlretrieve(last_res['urls']['small'], "./images/"+filename)
-        print (words[i] + " Downloaded! ✔")
-    else:
-        row += 1
-        undefined_word.update_cell(row,1,f"{words[i]}")
-        undefined_word.update_cell(row,2,"Undefined")
-        print (colored(words[i] + " Undefined! X",'red'))
-    os.environ['start'] = str(i+1)
-    dotenv.set_key(dotenv_file, "start", os.environ.get('start'))
+    try:
+        img = []
+        req = requests.get("https://api.unsplash.com/search/photos?page=1&query="+words[i]+"&client_id=m64NRbuNadSCiuSf4wx_LbBhYe1nY6sdJIqkkPSEovA")
+        data = req.json()
+        filename = str(words[i]) + ".jpg"
+        for res in data['results']:
+            if res['height'] > res['width']:
+                img.append(res)
+            else: continue
+        if img != []:
+            last_res = max(img,key=lambda item:item['likes'])
+            urllib2.urlretrieve(last_res['urls']['small'], "./images/"+filename)
+            print (words[i] + " Downloaded! ✔")
+        else:
+            row += 1
+            undefined_word.update_cell(row,1,f"{words[i]}")
+            undefined_word.update_cell(row,2,"Undefined")
+            print (colored(words[i] + " Undefined! X",'red'))
+        os.environ['start'] = str(i+1)
+        dotenv.set_key(dotenv_file, "start", os.environ.get('start'))
+    except:
+        print('waiting for 1 hour')
+        time.sleep(3900)
 
 print (colored('------------------\nSucessfully Download all your images!:)', 'green'))
